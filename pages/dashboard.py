e1c5acb797fefba19de477cce44e570dd3d8fbb5
Poojal Ello <ello.poojal@gmail.com>
2026-01-15 15:56:47 +0530

    Dashboard

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -7,14 +7,27 @@ class DashboardPage:
         self.driver = driver
         self.wait = WebDriverWait(driver, 10)
     
-    DASHBOARD_HEADING = (By.XPATH, "//*[contains(text(), 'Dashboard')]")
-    GREETING_MSG = (By.XPATH, "//span[contains(text(),'Good morning') or "
-                        "contains(text(),'Good afternoon') or "
-                        "contains(text(),'Good evening')]")
-    USER_LABEL = (By.XPATH, "//span[normalize-space()='Super Admin']")
+    DASHBOARD_HEADING = (By.XPATH, "//p[normalize-space()='Dashboard']")
+    # GREETING_MSG = (By.XPATH, "//span[contains(text(),'Good morning') or "
+    #                     "contains(text(),'Good afternoon') or "
+    #                     "contains(text(),'Good evening')]")
+    GREETING_MSG = (By.XPATH, "//span[contains(text(),'Good')]")
+    USER_LABEL = (By.XPATH, "//div[contains(@class,'ant-dropdown-trigger')]//span")
+    TOTAL_ORG = (By.XPATH, "//p[normalize-space()='Total Organizations']")
+    TOTAL_USERS = (By.XPATH, "//p[normalize-space()='Total Users']")
+    ORG_COUNT = (By.XPATH, "//p[normalize-space()='Total Organizations']"
+        "/ancestor::div[contains(@class,'border')]"
+        "//p[@class='text-2xl self-center leading-none font-bold']")
+    USERS_COUNT = (By.XPATH, "//p[normalize-space()='Total Users']"
+        "/ancestor::div[contains(@class,'border')]"
+        "//p[@class='text-2xl self-center leading-none font-bold']")
+
+
+    def get_username_text(self):
+        return self.wait.until(EC.visibility_of_element_located(self.USER_LABEL)).text
+
+    def is_element_visible(self, locator):
+        return self.wait.until(EC.visibility_of_element_located(locator)).is_displayed()
+        
 
     
-    def is_element_visible(self, locator):
-        return self.wait.until(
-            EC.visibility_of_element_located(locator)
-        ).is_displayed()
--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -13,6 +13,8 @@ def test_dashboard_loaded(logged_in_driver):
     # assert login_page.is_toast_message_displayed("Login Successfully")
 
     page = DashboardPage(logged_in_driver)
+    username = page.get_username_text()
+    assert username != ""
 
     assert page.is_element_visible(DashboardPage.DASHBOARD_HEADING)
     logging.info("Dashboard Heading is visible")
@@ -20,3 +22,13 @@ def test_dashboard_loaded(logged_in_driver):
     logging.info("Greetings message is visible")
     assert page.is_element_visible(DashboardPage.USER_LABEL)
     logging.info("User label is visible")
+
+
+    assert page.is_element_visible(DashboardPage.TOTAL_ORG)
+    logging.info("Total Organisations headiing is visible")
+    assert page.is_element_visible(DashboardPage.ORG_COUNT)
+    logging.info("Total Org count is visible")
+    assert page.is_element_visible(DashboardPage.TOTAL_USERS)
+    logging.info("Total Users headiing is visible")
+    assert page.is_element_visible(DashboardPage.USERS_COUNT)
+    logging.info("Total Users count is visible")
